f5473fa091bc3ebea345d138c848321c8d54b4b9
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-09 22:43:35 -0400

    Expose standardized GW store API

---
+++ src/darksirens/gw/__init__.py
@@ -0,0 +1,22 @@
+"""Standardized gravitational-wave posterior and selection data."""
+
+from .types import GWStore, SelectionStore
+from .samples import (
+    load_events,
+    load_gw_samples,
+    load_gw_store,
+    load_injections,
+    load_selection_samples,
+    load_selection_store,
+)
+
+__all__ = [
+    "GWStore",
+    "SelectionStore",
+    "load_events",
+    "load_injections",
+    "load_gw_store",
+    "load_selection_store",
+    "load_gw_samples",
+    "load_selection_samples",
+]
