be619bf29ec80efa6c0fdbbe55c2702f7a224f7a
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-10 02:18:53 -0400

    Export spectral siren likelihood

--- src/darksirens/likelihood/__init__.py
+++ src/darksirens/likelihood/__init__.py
@@ -1,5 +1,6 @@
 """Core hierarchical-likelihood primitives."""
 
+from .hierarchical import SpectralLikelihoodDiagnostics, spectral_siren_log_likelihood
 from .weights import (
     M1DET_Q_DL_COORDS,
     log_jacobian_dL_to_z,
@@ -10,8 +11,10 @@ from .weights import (
 
 __all__ = [
     "M1DET_Q_DL_COORDS",
+    "SpectralLikelihoodDiagnostics",
     "log_jacobian_dL_to_z",
     "log_jacobian_m1src_q_z_to_m1det_q_dL",
     "log_sample_weight",
     "log_target_density_m1det_q_dL",
+    "spectral_siren_log_likelihood",
 ]
