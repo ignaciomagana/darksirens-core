12f14dbc05aed12ab9fc18d7d465279f15349bf1
Ignacio Magana <imhernan@andrew.cmu.edu>
2026-09-09 23:41:47 -0400

    Expose population model registry

--- src/darksirens/population/__init__.py
+++ src/darksirens/population/__init__.py
@@ -1,12 +1,13 @@
-# populations/__init__.py
+"""Population models and registry for hierarchical siren inference."""
+
 from .registry import (
-    get_model,
-    pop_model_parser,
-    pop_model_prior_parser,
     FIDUCIAL_SETS,
     FIDUCIAL_SET_IN_PRIOR,
     FIDUCIAL_SET_LEGACY,
     get_fixed_population_params,
+    get_model,
+    pop_model_parser,
+    pop_model_prior_parser,
     population_m1_support_max,
 )
 
